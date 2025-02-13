3871a87dff46f48961bc950a07b27650d47bbfc1
vineeth shettigar <90184123+Vineeth0102@users.noreply.github.com>
2025-02-13 08:08:03 +0530

    Update pro1.py

--- This Year/Aug 16,2024 Slot-2/pro1.py
+++ This Year/Aug 16,2024 Slot-2/pro1.py
@@ -2,6 +2,7 @@
 Given an array with 'N' Elements you are expected to find the sum of the values that are present in the prime index of the array note that the array index starts with 0 i.e the position (index) on the array element is 0, the position os the next element is 1 and so on
 
 input : N = 10, arr [] = [1,2,3,4,5,6,7,8,9,10]
+
 output : 
 
 """
